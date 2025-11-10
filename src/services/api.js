80250a3f2fdceb14ca185e807f7844eecc2fbef9
Edusan Foundation <edusan.org.in>
2025-11-10 15:17:53 +0530

    changes in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://slotswapperbackend-imr5.onrender.com/api"
+    : "http://localhost:5000/api";
 
 export const API = {
   async request(endpoint, options = {}) {
